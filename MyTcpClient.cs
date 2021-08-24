9f5f33e34b05471b425f8f72058fe11e148980ae
Anish Punaroor <anish.punaroor@gmail.com>
2021-08-24 15:19:51 -0700

    Read and display data

--- MyTcpClient.cs
+++ MyTcpClient.cs
@@ -12,5 +12,29 @@ class MyTcpClient
         _endPoint = new IPEndPoint(_address, _port); 
     }
 
+    public void ConnectToServer(string msg); 
+    {
+        try 
+        {
+            _client = new TcpClient(); 
+            _client.Connect(_endPoint); 
+
+
+            byte[] bytes = Encoding.ASCII.GetBytes(msg);
+            using (NetworkStream ns = _client.GetStream()) 
+            {
+                // Send the message. 
+                Trace.WriteLine("Sending message to server: " + msg); 
+                ns.Write(bytes, 0, bytes.Length); 
+
+                bytes = new byte[1024]; 
+
+                // Display response. 
+                int bytesRead = ns.Read(bytes, 0, bytes.Length); 
+                string ServerResponse = Encoding.ASCII.GetString(bytes, 0, bytesRead); 
+                Trace.WriteLine("Server said: " + serverResponse); 
+            }
+        }
+    }
 
 }
